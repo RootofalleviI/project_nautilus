04433703c6fca7c7e001c62a67df9af89f41897c
rootofallevii <lastwhisper98@gmail.com>
2019-01-03 13:51:30 -0500

    Add and read daily record

--- V2.0/main.py
+++ V2.0/main.py
@@ -1,8 +1,10 @@
 import cmd
 import os
-from datetime import date, time, datetime, timedelta
+from datetime import date
 from pathlib import Path
 import pandas as pd
+from tabulate import tabulate
+import numpy as np
 
 
 HOME = str(Path.home())
@@ -11,6 +13,8 @@ DATA_CENTER = PROJECT_ROOT_URL + "/.data/daily"
 
 DEBUG = True
 
+today_as_str = date.today().strftime('%Y-%m-%d,%A')
+today_record_path = DATA_CENTER + f'/{today_as_str}.csv'
 
 class Interpreter(cmd.Cmd):
 
@@ -21,48 +25,46 @@ class Interpreter(cmd.Cmd):
         """Make sure relevant directories have been mkdir'ed."""
         if not os.path.exists(DATA_CENTER):
             os.makedirs(DATA_CENTER)
-
-    def do_init(self, _):
-        """`init`: initialize today's record."""
-        today = date.today()
-        today_as_str = today.strftime('%Y-%m-%d,%A')
-        file_path = DATA_CENTER + f'/{today_as_str}.csv'
-
-        if os.path.isfile(file_path):
-            debug("File exists.")
-            print("Today's record has been initialized already.")
-        else:
-            debug(f"Initializing {file_path}")
-            init_record(file_path)
-            print("Today's record has been initialized successfully.")
+        if not os.path.isfile(today_record_path):
+            start = [format_time(x) for x in list(pd.timedelta_range(0, periods=48, freq="30T"))]
+            finish = start[1:] + ['00:00']
+            columns = ['start', 'finish', 'category', 'title', 'description']
+            df = pd.DataFrame(index=range(1, 49), columns=columns)
+            df['start'], df['finish'] = start, finish
+            df.to_csv(today_record_path)
+        self.df = pd.read_csv(today_record_path, index_col=0)
+        self.df.fillna('None', inplace=True)
 
     def do_add(self, args):
         """`add <start> <finish> <title> <description>"""
         arg_lst = args.split(' ', 3)
-        print(arg_lst)
+        if len(arg_lst) != 4:
+            print("Error: Wrong number of arguments. \nUsage: `add <start> <finish> <title> <description>")
+        start, finish, title, description = arg_lst
+        start_idx = int(start[:2]) * 2 + 1 + (start[2:] == '30')
+        finish_idx = int(finish[:2]) * 2 + 1 + (finish[2:] == '30')
+        for x in range(start_idx, finish_idx):
+            if self.df.loc[x,'title'] != 'None':
+                print("title assigned.")
+            else:
+                self.df.loc[x,'title'] = title
+            if self.df.loc[x,'description'] != 'None':
+                print("desc assigned")
+            else:
+                self.df.loc[x,'description'] = description
+        print(self.df)
+        print(self.df.dtypes)
+
+    def do_read(self, _):
+        print(tabulate(self.df, headers=['id', 'start', 'finish', 'category', 'title', 'description']))
+        print(self.df.dtypes)
 
     def do_bye(self, _):
         print("Bye")
         return True
 
-def debug(s):
-    if DEBUG:
-        print(f"DEBUG: {s}")
-
-
-def format_time(timeindex):
-    return ':'.join(str(timeindex).split()[-1].split(':')[:2])
-
-
-def init_record(file_path):
-    start = [format_time(x) for x in list(pd.timedelta_range(0, periods=48, freq="30T"))]
-    finish = start[1:] + ['00:00']
-    columns = ['start', 'finish', 'category', 'title', 'description']
-    df = pd.DataFrame(index=range(1, 49), columns=columns)
-    df['start'], df['finish'] = start, finish
-    df.to_csv(file_path)
-
-
+def format_time(i):
+    return ':'.join(str(i).split()[-1].split(':')[:2])
 
 if __name__ == '__main__':
     Interpreter().cmdloop()
